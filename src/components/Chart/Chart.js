4cb7b63e7262cdf562319bb704a31b53ab36f71a
shaikhreza43 <reza.rk786@gmail.com>
2020-11-19 02:27:19 +0530

    Hosted on Firebase

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import BaseLayoutWrapper from '../BaseLayoutWrapper/BaseLayoutWrapper';
 import { Card, CardBody, CardTitle, Col, Row } from 'reactstrap';
-import ReactApexChart from 'react-apexcharts'
+import ReactApexChart from 'react-apexcharts';
+import { LinearProgress, Typography } from '@material-ui/core';
 
 const Chart = (props) => {
 
@@ -59,6 +60,176 @@ const Chart = (props) => {
 
     });
 
+    const [pieChart, setPieChart] = useState({
+        optionsRadial: {
+            plotOptions: {
+                radialBar: {
+                    startAngle: -135,
+                    endAngle: 225,
+                    hollow: {
+                        margin: 0,
+                        size: "70%",
+                        background: "#fff",
+                        image: undefined,
+                        imageOffsetX: 0,
+                        imageOffsetY: 0,
+                        position: "front",
+                        dropShadow: {
+                            enabled: true,
+                            top: 3,
+                            left: 0,
+                            blur: 4,
+                            opacity: 0.24
+                        }
+                    },
+                    track: {
+                        background: "#fff",
+                        strokeWidth: "67%",
+                        margin: 0, // margin is in pixels
+                        dropShadow: {
+                            enabled: true,
+                            top: -3,
+                            left: 0,
+                            blur: 4,
+                            opacity: 0.35
+                        }
+                    },
+
+                    dataLabels: {
+                        showOn: "always",
+                        name: {
+                            offsetY: -20,
+                            show: true,
+                            color: "#888",
+                            fontSize: "13px"
+                        },
+                        value: {
+                            formatter: function (val) {
+                                return val;
+                            },
+                            color: "#111",
+                            fontSize: "30px",
+                            show: true
+                        }
+                    }
+                }
+            },
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shade: "dark",
+                    type: "horizontal",
+                    shadeIntensity: 0.5,
+                    gradientToColors: ["#ABE5A1"],
+                    inverseColors: true,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 100]
+                }
+            },
+            stroke: {
+                lineCap: "round"
+            },
+            labels: ["Percent"]
+        },
+        seriesRadial: [76]
+    });
+
+
+    const [series, setSeries] = useState({
+        series: [{
+            name: "Desktops",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+        }],
+        options: {
+            chart: {
+                height: 350,
+                type: 'line',
+                zoom: {
+                    enabled: false
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'straight'
+            },
+            title: {
+                text: 'Product Trends by Month',
+                align: 'left'
+            },
+            grid: {
+                row: {
+                    colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                    opacity: 0.5
+                },
+            },
+            xaxis: {
+                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
+            }
+        }
+    });
+
+    const [sales, setSales] = useState({
+
+        series: [{
+            data: [21, 22, 10, 28, 16, 21, 13, 30]
+        }],
+        options: {
+            chart: {
+                height: 350,
+                type: 'bar',
+                events: {
+                    click: function (chart, w, e) {
+                        // console.log(chart, w, e)
+                    }
+                }
+            },
+            colors: [
+                "#eeeeee",
+                "#0000FF"
+            ],
+            plotOptions: {
+                bar: {
+                    columnWidth: '45%',
+                    distributed: true
+                }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            legend: {
+                show: false
+            },
+            xaxis: {
+                categories: [
+                    ['John', 'Doe'],
+                    ['Joe', 'Smith'],
+                    ['Jake', 'Williams'],
+                    'Amber',
+                    ['Peter', 'Brown'],
+                    ['Mary', 'Evans'],
+                    ['David', 'Wilson'],
+                    ['Lily', 'Roberts'],
+                ],
+                labels: {
+                    style: {
+                        colors: [
+                            "#eeeeee",
+                            "#0000FF"
+                        ],
+                        fontSize: '12px'
+                    }
+                }
+            }
+        },
+
+
+
+    });
+
+
     return (
         <BaseLayoutWrapper>
             <Row>
@@ -80,6 +251,13 @@ const Chart = (props) => {
                                 Technologies
                             </CardTitle>
 
+                            <ReactApexChart
+                                options={pieChart.optionsRadial}
+                                series={pieChart.seriesRadial}
+                                type="radialBar"
+                                width="350"
+                            />
+
                         </CardBody>
                     </Card>
                 </Col>
@@ -90,6 +268,7 @@ const Chart = (props) => {
                             <CardTitle tag="h5">
                                 Total Orders
                             </CardTitle>
+                            <ReactApexChart options={series.options} series={series.series} type="line" height={350} />
 
                         </CardBody>
                     </Card>
@@ -104,7 +283,14 @@ const Chart = (props) => {
                             <CardTitle tag="h5">
                                 Announcements
                             </CardTitle>
-
+                            <Typography variant="h6">
+                                Lorem Ipsum is simply dummy text
+                        </Typography>
+                            <p className="text-justify">
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            </p>
                         </CardBody>
                     </Card>
                 </Col>
@@ -115,7 +301,15 @@ const Chart = (props) => {
                             <CardTitle tag="h5">
                                 Projects
                             </CardTitle>
-
+                            <Typography variant="h6">
+                                First Milestone achieved
+                        </Typography>
+                            <p className="text-justify">
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            </p>
+                            <LinearProgress variant="determinate" value={75}></LinearProgress>
                         </CardBody>
                     </Card>
                 </Col>
@@ -126,7 +320,14 @@ const Chart = (props) => {
                             <CardTitle tag="h5">
                                 Today's Schedule
                             </CardTitle>
-
+                            <Typography variant="h6">
+                                UI/Ux Design Update's
+                        </Typography>
+                            <Typography variant="h6">
+                                <p className="font-weight-bold">
+                                    11:15AM-12:45PM
+                               </p>
+                            </Typography>
                         </CardBody>
                     </Card>
                 </Col>
@@ -151,7 +352,7 @@ const Chart = (props) => {
                             <CardTitle tag="h5">
                                 Sales Statistics
                             </CardTitle>
-
+                            <ReactApexChart options={sales.options} series={sales.series} type="bar" height={350} />
                         </CardBody>
                     </Card>
                 </Col>
